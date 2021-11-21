9ceeb30fde12d81e6f80931586f8664795adbe01
Boody <cfbuddy124@gmail.com>
2021-11-21 23:44:10 +0200

    Add fields to login form

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,10 +1,19 @@
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
-      <from>
-        <input type="text" />
-      </from>
+      <label>Login</label>
+      <form>
+        <label for="firstName">First name</label>
+        <input type="text" id="firstName" />
+        <label for="lastName">Last name</label>
+        <input type="text" id="lastName" />
+        <label for="phoneOrEmail">Mobile number or email</label>
+        <input type="text" id="phoneOrEmail" />
+        <label for="password">Password</label>
+        <input type="text" id="password" />
+        <label for="confirmPassword">Re-enter password</label>
+        <input type="text" id="confirmPassword" />
+      </form>
     </div>
   );
 };
