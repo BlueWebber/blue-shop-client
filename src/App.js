53202df80f478ec14dbc2c9d772e7b553737d078
Boody <cfbuddy124@gmail.com>
2021-11-22 02:25:45 +0200

    Integrate global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./screens/login";
 import { ThemeProvider } from "./context/theme";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <ThemeProvider>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <GlobalStyle>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </GlobalStyle>
     </ThemeProvider>
   );
 }
