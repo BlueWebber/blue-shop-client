fd28b7223902a39c10cf45cb0e74f2cda0fed727
Boody <cfbuddy124@gmail.com>
2021-11-22 02:10:02 +0200

    Integrate theme into the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./screens/login";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
