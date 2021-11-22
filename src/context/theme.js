f396c9c67f7d10b6396a2fa71fe16ccf566a95e3
Boody <cfbuddy124@gmail.com>
2021-11-22 02:04:03 +0200

    Add theme context

---
+++ src/context/theme.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyledThemeProvider } from "styled-components";
+import darkTheme from "../styles/themes/dark";
+import lightTheme from "../styles/themes/light";
+
+const ThemeSwitcherContext = React.createContext();
+ThemeSwitcherContext.displayName = "ThemeSwitcherContext";
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = React.useState("dark");
+
+  return (
+    <ThemeSwitcherContext.Provider value={[theme, setTheme]}>
+      <StyledThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+        {children}
+      </StyledThemeProvider>
+    </ThemeSwitcherContext.Provider>
+  );
+};
+
+const useTheme = () => React.useContext(ThemeSwitcherContext);
+
+export { ThemeProvider, useTheme };
