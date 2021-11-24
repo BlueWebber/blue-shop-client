9a0b93686b119852a665948c265b567f70e4459b
Boody <cfbuddy124@gmail.com>
2021-11-24 21:19:22 +0200

    Update theme provider from MUI

--- src/context/theme.js
+++ src/context/theme.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import darkTheme from "../styles/themes/dark";
-import lightTheme from "../styles/themes/light";
+// import { ThemeProvider as StyledThemeProvider } from "styled-components";
+// import darkTheme from "../styles/themes/dark";
+// import lightTheme from "../styles/themes/light";
+// import useMediaQuery from '@mui/material/useMediaQuery';
+import {
+  createTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material/styles";
+// import CssBaseline from '@mui/material/CssBaseline';
 
 const ThemeSwitcherContext = React.createContext();
 ThemeSwitcherContext.displayName = "ThemeSwitcherContext";
-
+/*
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = React.useState("dark");
 
@@ -14,6 +20,27 @@ const ThemeProvider = ({ children }) => {
       <StyledThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
         {children}
       </StyledThemeProvider>
+    </ThemeSwitcherContext.Provider>
+  );
+};
+*/
+
+const ThemeProvider = ({ children }) => {
+  // useMediaQuery('(prefers-color-scheme: dark)');
+  const [theme, setTheme] = React.useState("dark");
+  const createdTheme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: theme,
+        },
+      }),
+    [theme]
+  );
+
+  return (
+    <ThemeSwitcherContext.Provider value={[theme, setTheme]}>
+      <MuiThemeProvider theme={createdTheme}>{children}</MuiThemeProvider>
     </ThemeSwitcherContext.Provider>
   );
 };
