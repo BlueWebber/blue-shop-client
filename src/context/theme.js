3f5def71b1bf0a8067138f4fbf485afdeafedc11
Boody <cfbuddy124@gmail.com>
2021-11-28 02:38:41 +0200

    finish theme provider

--- src/context/theme.js
+++ src/context/theme.js
@@ -7,23 +7,10 @@ import {
   createTheme,
   ThemeProvider as MuiThemeProvider,
 } from "@mui/material/styles";
-// import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
 
 const ThemeSwitcherContext = React.createContext();
 ThemeSwitcherContext.displayName = "ThemeSwitcherContext";
-/*
-const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = React.useState("dark");
-
-  return (
-    <ThemeSwitcherContext.Provider value={[theme, setTheme]}>
-      <StyledThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
-        {children}
-      </StyledThemeProvider>
-    </ThemeSwitcherContext.Provider>
-  );
-};
-*/
 
 const ThemeProvider = ({ children }) => {
   // useMediaQuery('(prefers-color-scheme: dark)');
@@ -40,6 +27,7 @@ const ThemeProvider = ({ children }) => {
 
   return (
     <ThemeSwitcherContext.Provider value={[theme, setTheme]}>
+      <CssBaseline />
       <MuiThemeProvider theme={createdTheme}>{children}</MuiThemeProvider>
     </ThemeSwitcherContext.Provider>
   );
