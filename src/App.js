65267b1fa24d72c6e4e1ed50c10098538ec39ea7
Boody <cfbuddy124@gmail.com>
2021-12-07 02:05:27 +0200

    Replace temporary app wrapper with Main

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./screens/login";
 import AppWrapper from "./components/appWrapper";
 import AppBar from "./components/appBar";
+import Main from "./components/main";
 
 function App() {
   return (
     <AppWrapper>
-      <div
-        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
-      >
+      <Main>
         <AppBar />
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      </div>
+      </Main>
     </AppWrapper>
   );
 }
