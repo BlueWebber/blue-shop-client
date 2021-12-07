5de6aca803c6fff3bbab17dca6496b7b6ffa9d7a
Boody <cfbuddy124@gmail.com>
2021-12-07 12:43:22 +0200

    Create appDrawer component

---
+++ src/components/appDrawer.jsx
@@ -0,0 +1,13 @@
+import { Drawer } from "@mui/material";
+import { useAppDrawer } from "../context/appDrawer";
+
+const AppDrawer = () => {
+  const [drawerOn, setDrawerOn] = useAppDrawer();
+  return (
+    <Drawer anchor="left" open={drawerOn} onClose={setDrawerOn(false)}>
+      hi this is an app drawer
+    </Drawer>
+  );
+};
+
+export default AppDrawer;
