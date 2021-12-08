17ad1dcf10cc0cbb7f4a2b40eba1502f1261b385
Boody <cfbuddy124@gmail.com>
2021-12-08 20:57:06 +0200

    Create and integrate sign up form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./screens/login";
+import Signup from "./screens/signup";
 import AppWrapper from "./components/appWrapper";
 import NavBar from "./components/navBar";
 import AppDrawer from "./components/appDrawer";
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Main>
     </AppWrapper>
---
+++ src/screens/signup.jsx
@@ -0,0 +1,71 @@
+import FormBox from "../components/common/formBox";
+import { Typography, TextField, Button, Box, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
+const Signup = () => {
+  return (
+    <FormBox>
+      <Typography align="center" variant="h6" component="h1">
+        Sign Up
+      </Typography>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <TextField
+          type="text"
+          id="phoneOrEmail"
+          variant="outlined"
+          label="Phone number or email"
+          margin="normal"
+          required
+          fullWidth
+        />
+        <TextField
+          type="text"
+          id="firstName"
+          variant="outlined"
+          label="First Name"
+          margin="normal"
+          required
+          fullWidth
+        />
+        <TextField
+          type="text"
+          id="lastName"
+          variant="outlined"
+          label="Last Name"
+          margin="normal"
+          required
+          fullWidth
+        />
+
+        <TextField
+          type="password"
+          id="password"
+          variant="outlined"
+          label="Password"
+          margin="normal"
+          required
+          fullWidth
+        />
+        <TextField
+          type="password"
+          id="confirmPassword"
+          variant="outlined"
+          label="Confirm password"
+          margin="normal"
+          required
+          fullWidth
+        />
+        <Button variant="contained" fullWidth sx={{ mt: 1 }} type="submit">
+          Submit
+        </Button>
+      </form>
+      <Box sx={{ mt: 1, textAlign: "center" }}>
+        <Link component={RouterLink} underline="none" to="/login">
+          Already have an account? Sign in
+        </Link>
+      </Box>
+    </FormBox>
+  );
+};
+
+export default Signup;
