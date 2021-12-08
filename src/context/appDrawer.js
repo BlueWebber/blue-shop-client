e1b2693720ca167e256d0cb935db4d451c1b42b8
Boody <cfbuddy124@gmail.com>
2021-12-08 19:16:56 +0200

    Make app drawer get closed on url change

--- src/context/appDrawer.js
+++ src/context/appDrawer.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 const DrawerContext = React.createContext();
 DrawerContext.displayName = "appDrawerContext";
 
 const AppDrawerProvider = ({ children }) => {
   const [drawerOn, setDrawerOn] = React.useState(false);
+  const location = useLocation();
+  React.useEffect(() => setDrawerOn(false), [location]);
+
   return (
     <DrawerContext.Provider value={[drawerOn, setDrawerOn]}>
       {children}
