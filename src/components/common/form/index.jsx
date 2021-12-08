0218c1409d661fa2734e0c43dd15c675baeb10ec
Boody <cfbuddy124@gmail.com>
2021-12-08 21:14:25 +0200

    fix the index file in the 'form' folder

--- src/components/common/form/index.jsx
+++ src/components/common/form/index.jsx
@@ -1,4 +1,4 @@
-export * from "./formBox";
-export * from "./formLink";
-export * from "./formTitle";
-export * from "./formSubmitButton";
+export { default as FormBox } from "./formBox";
+export { default as FormLink } from "./formLink";
+export { default as FormTitle } from "./formTitle";
+export { default as FormSubmitButton } from "./formSubmitButton";
