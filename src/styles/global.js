c2c6a8364c686c3132d0cc3ee3ab7f1ce59f7eec
Boody <cfbuddy124@gmail.com>
2021-12-10 01:49:27 +0200

    Add custom scrollbar

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,74 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 
-// styles you might need in the future
-/*
-  * {
-    margin: 0;
-    padding: 0;
-  }
-
-  :root {
-    background-color: ${({ theme }) => theme.colors.appBackground.main};
-  }
-
-  html, body, #root {
-    @media only screen and (max-width: 600px), 
-      only screen and (max-height: 700px) {
-      height: 100%;
-    };
-  }
-
-  main {
-    @media only screen and (max-width: 600px) {
-      height: 85%;
-      margin-top: 5px;
-    }
-    @media only screen and (max-height: 600px) {
-      height: 97%;
-      padding-top: 5px;
-    }
-  }
-
-  body {
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-    line-height: 1.5;
-    font-size: 16px;
-    color: ${({ theme }) => theme.colors.text.primary};
-  }
-  main {
-    display: flex;
-    flex-grow: 1;
-    justify-content: center;
-  }
-  a {
-    color: ${({ theme }) => theme.colors.text.primary};
-    text-decoration: none;
-  }
-  button {
-    transition: 0.15s ease-in-out;
-    &:hover {
-      cursor: pointer;
-    }
-  }
-  */
-
 const GlobalStyle = createGlobalStyle`
-    /*
-    .flexColumn {
-        display: flex;
-        flex-direction: column;
-    }
+  /* Scroll Width */
+  ::-webkit-scrollbar {
+    width: 7px;
+    height: 7px;
+  }
 
-    .flexRow {
-        display: flex;
-        flex-direction: row;
-    }
+  /* Scroll Track */
+  ::-webkit-scrollbar-track {
+    background: "inherit";
+  }
 
-    form {
-        display: flex;
-        flex-direction: column;
-    }
-    */
+  /* Scroll Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #686868;
+    border-radius: 10px;
+    margin-top: 5px;
+  }
+
+  /* Scroll on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #7b7b7b;
+  }
 `;
 
 export default GlobalStyle;
