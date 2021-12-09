596bf5201e0353372f11fcca2795c8c6326f1ea7
Boody <cfbuddy124@gmail.com>
2021-12-10 01:48:56 +0200

    Add loading circle delay to prevent loading component flashes

--- src/components/common/componentLoading.jsx
+++ src/components/common/componentLoading.jsx
@@ -1,6 +1,14 @@
 import { CircularProgress } from "@mui/material";
+import React from "react";
 
-const ComponentLoading = ({ center, size = 52 }) => {
+const ComponentLoading = ({ center, size = 52, delay = 500 }) => {
+  const [visible, setVisible] = React.useState(false);
+  React.useEffect(() => {
+    setTimeout(() => setVisible(true), delay);
+  }, [delay]);
+
+  if (!visible) return <></>;
+
   return (
     <CircularProgress
       color="inherit"
