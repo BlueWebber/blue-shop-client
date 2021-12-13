63cb2bd4fdeaa44178c352007de8621b3b0b2a43
Boody <cfbuddy124@gmail.com>
2021-12-13 04:00:39 +0200

    Remove the theme changer button from the appBar

--- src/components/appBar.jsx
+++ src/components/appBar.jsx
@@ -1,16 +1,8 @@
-import {
-  AppBar as AppBarComp,
-  Toolbar,
-  Button,
-  IconButton,
-} from "@mui/material";
+import { AppBar as AppBarComp, Toolbar, Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import Brightness6Icon from "@mui/icons-material/Brightness6";
-import { useTheme } from "../context/theme";
 import logo from "../assets/images/logoHorizontal.png";
 
 const AppBar = () => {
-  const [theme, setTheme] = useTheme();
   return (
     <AppBarComp
       position="static"
@@ -28,14 +20,6 @@ const AppBar = () => {
         <Button component={Link} to="">
           <img src={logo} alt="blue shop's website logo" height="30px" />
         </Button>
-        <IconButton
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          sx={{
-            color: (theme) => theme.palette.text.navButton,
-          }}
-        >
-          <Brightness6Icon />
-        </IconButton>
       </Toolbar>
     </AppBarComp>
   );
